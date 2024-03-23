06f9158f54f01c1bcdd0b51c7f4674ae1f3b692b
Vinicius Pinheiro <vinnie.pine@mail.com>
2024-03-23 19:00:34 -0400

    first commit

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleList
+{
+    internal class Product
+    {
+        public String name;
+        public decimal value;
+        public string ID;
+        public decimal price;
+        public decimal weight;
+        public string category;
+        public Product? next;
+        // O "?" (o sinal de interrogação) serve de "null safe"; garante que o
+        // o programa continue operando ainda que o valor seja nulo.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Instituto Federal de Educação, Ciência e Tecnologia de Rondônia - IFRO
+ *  C.S.T. em Análise e Desenvolvimento de Sistemas
+ *  Disciplina de Algoritmo e Lógica de Programação II
+ *  Professor Clayton Ferraz de Andrade
+ *  
+ *  Acadêmico Vinicius de Oliveira Pinheiro
+ *  Aula do dia 09-10-2023
+ *  - Introdução do conceito de classe
+ *  - Breve apresentação do conceito de programação orientada a objetos
+ */
+using SimpleList;
+
+Product myProduct  = new Product();
+Console.WriteLine("Digite o nome do produto: ");
+myProduct.name = Console.ReadLine();
+Console.WriteLine("Digite o valor do produto: ");
+myProduct.value = Convert.ToDecimal(Console.ReadLine());
